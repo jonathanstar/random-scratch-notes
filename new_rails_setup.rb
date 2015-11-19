d91cbbe1409a707de9b2cc0ea6916ddc35dfd552
Jon Star <jonathanstar@users.noreply.github.com>
2015-11-19 17:51:13 -0500

    Update new_rails_setup.rb

--- new_rails_setup.rb
+++ new_rails_setup.rb
@@ -18,9 +18,51 @@ rails g rspec:install
 git add .
 git commit -m "Initialize rspec."
 
+# in rails helper change the following assignment from true to false
+
+config.use_transactional_fixtures = false
+
+# uncomment the following line in spec/rails_helper.rb
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+#create a database cleaner initializer 
+
+touch config/database_cleaner.rb
+
+# add this code to database_cleaner.rb
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
+
+# uncomment 
+
 #Create the repo on github. Don't add a .gitignore or license.
 
 git remote add origin https://github.com/your_organization/my_great_app.git
 git push -u origin master
 
 #Only now start adding your own code. Apart from ensuring all your tools are in place, doing all the install steps up front gives you a good point to compare back to later
+
+
+# when you want test coverage you must add the following to spec/rails_helper
