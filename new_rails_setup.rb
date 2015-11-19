40d5ffcfc130548d04e5b405125d575a40e0c9fe
Jon Star <jonathanstar@users.noreply.github.com>
2015-11-19 17:58:55 -0500

    Update new_rails_setup.rb

--- new_rails_setup.rb
+++ new_rails_setup.rb
@@ -13,6 +13,21 @@ git commit -m "Enable bcrypt."
 #Edit Gemfile. Add gem rspec-rails (and probably faker and factory_girl_rails if wanted) to the development, test group toward the bottom of the Gemfile.
 # add 'rspec-rails' to gemfile
 
+# suggessted gems
+
+=begin
+  gem 'pry-rails'
+  gem 'rspec-rails'
+  gem 'faker'
+  gem 'shoulda-matchers'
+  gem 'factory_girl_rails'
+  gem 'capybara'
+  gem 'launchy'
+  gem 'database_cleaner'
+  gem 'poltergeist'
+=end
+
+
 bundle install
 rails g rspec:install
 git add .
@@ -25,6 +40,10 @@ config.use_transactional_fixtures = false
 # uncomment the following line in spec/rails_helper.rb
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+# if you are using capybara, add this to spec/rails_helper.rb
+require 'capybara/rails'
+
 
 #create a database cleaner initializer 
 
@@ -55,7 +74,29 @@ RSpec.configure do |config|
 
 end
 
-# uncomment 
+require 'capybara/rails'
+# when you want test coverage you must add the following to spec/rails_helper
+
+#if you are using simplecov, add the following to spec/rails_helper.rb
+
+require 'simplecov'
+# Add additional requires below this line. Rails is not loaded until this point!
+# save to CircleCI's artifacts directory if we're on CircleCI
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+
+SimpleCov.start
+
+# if you are using shouldaMatchers, create a file called spec/support/shoulda.rb with the following content:
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 
 #Create the repo on github. Don't add a .gitignore or license.
 
@@ -63,6 +104,3 @@ git remote add origin https://github.com/your_organization/my_great_app.git
 git push -u origin master
 
 #Only now start adding your own code. Apart from ensuring all your tools are in place, doing all the install steps up front gives you a good point to compare back to later
-
-
-# when you want test coverage you must add the following to spec/rails_helper
